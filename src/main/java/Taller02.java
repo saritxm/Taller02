c15e2136823213e68445add9e0d96e924d0e36a5
saritxm <saranicollemm@gmail.com>
2024-03-05 14:24:57 -0500

    first commit

---
+++ src/main/java/Taller02.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Sara
+ */
+public class Taller02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
