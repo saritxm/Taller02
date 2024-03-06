8a06c7864fddd299720248271b32ed82eea51f08
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-03-06 11:39:54 -0500

    a

---
+++ target/classes/model/Player.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Player {
+    private String nombre;
+    private int edad;
+    private String cedula;
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCedula() {
+        return this.cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+}
