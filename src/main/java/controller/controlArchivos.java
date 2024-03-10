3d5f8391dfd72e625c19b0bfa2d6ac0b9af62270
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-03-09 22:11:05 -0500

    Propierties

--- src/main/java/controller/controlArchivos.java
+++ src/main/java/controller/controlArchivos.java
@@ -1,9 +1,12 @@
 package controller;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Properties;
+
+import javax.swing.JFileChooser;
 
 import model.Team;
 
@@ -11,29 +14,33 @@ public class controlArchivos {
     private ArrayList<Team> equipos;
     private Properties prop = new Properties();
 
-    public controlArchivos(){
+    public controlArchivos() throws IOException{
         this.equipos = new ArrayList<>();
     }
 
-    public void obtenerEquipos(String ruta) throws IOException{
+    public void obtenerEquipos(File f) throws IOException{
         try {
-            prop.load(new FileInputStream("data/teams.properties"));
+            prop.load(new FileInputStream(f));
             ArrayList<String> listaEq = new ArrayList<>();
             for(String key : prop.stringPropertyNames()){
                 if(key.startsWith("team")){
-                    listaEq.add(key);
+                    if(!listaEq.contains(key.split("\\.")[0])) listaEq.add(key.split("\\.")[0]);                  
                 }
             }
             for(String i : listaEq){
                 String nombre = prop.getProperty(i+".name");
                 String dep = prop.getProperty(i+".name");
                 int num = Integer.parseInt(prop.getProperty(i+".num"));
-                equipos.add(new Team());
+                equipos.add(new Team(nombre, dep, num));
             }
 
         } catch (IOException e) {
             e.getStackTrace();        
         }
+    }
+
+    public ArrayList<Team> getEquipos() {
+        return this.equipos;
     }
     
 }
