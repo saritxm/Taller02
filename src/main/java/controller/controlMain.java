418a0889fd1e312baaaf56f7b4bcdd37752c3e04
saritxm <saranicollemm@gmail.com>
2024-03-09 23:15:24 -0500

    11:11

--- src/main/java/controller/controlMain.java
+++ src/main/java/controller/controlMain.java
@@ -4,18 +4,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import view.vBienvenida;
 import view.vJugador;
+import view.vPartida;
 
 public class controlMain implements ActionListener {
 
-    private vBienvenida vb;
-    private vJugador vj;
     private controlArchivos cArchivos;
     private controlPlayers cPlayers;
+    private vBienvenida vb;
+    private vJugador vj;
+    private vPartida vp;
+    
 
     public controlMain() {
         //Instanciacion de la vista
         this.vb = new vBienvenida();
         this.vj = new vJugador();
+        this.vp = new vPartida();
         //Escucha botones de la vista
         this.vb.btnContinuar.addActionListener(this);
 
@@ -24,7 +28,8 @@ public class controlMain implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == this.vb.btnContinuar){
-            
+            vb.setVisible(false);
+            vj.setVisible(true);  
         }
     }
 
