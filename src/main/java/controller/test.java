05693bbbb568a297234c1faacb2fc4087e892662
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-03-10 15:56:50 -0500

    Manejo de archivos OK

--- src/main/java/controller/test.java
+++ src/main/java/controller/test.java
@@ -1,24 +1,26 @@
 package controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JFileChooser;
 
 import model.*;
 
 public class test {
-    public static void main(String[] args) {
-        Team ganadores = new Team("Pedro","Quillami", 12);
+    public static void main(String[] args) throws IOException{
+        Team ganadores = new Team("Pedro Papa","Quillami", 12);
         Player wj1 = new Player();
-        wj1.setNombre("Pedro");
+        wj1.setNombre("juan");
         Player wj2 = new Player();
-        wj2.setNombre("Pedro");
+        wj2.setNombre("sara");
         Player wj3 = new Player();
-        wj3.setNombre("Pedro");
+        wj3.setNombre("camilo");
         Player wj4 = new Player();
-        wj4.setNombre("Pedro");
+        wj4.setNombre("xd");
         ganadores.Iniciar(wj1, wj2, wj3, wj4);
-        Team perdedores = new Team("Pedro","Quillami", 12);
+        Team perdedores = new Team("Pepo","Quillami", 12);
         Player pj1 = new Player();
         pj1.setNombre("Pedro");
         Player pj2 = new Player();
@@ -28,10 +30,48 @@ public class test {
         Player pj4 = new Player();
         pj4.setNombre("Pedro");
         perdedores.Iniciar(pj1, pj2, pj3, pj4);
-        Result aux = new Result(ganadores, perdedores);
+
+        Team ganadores2 = new Team("Pedro","Quillami", 12);
+        Player wj12 = new Player();
+        wj12.setNombre("Pedro");
+        Player wj22 = new Player();
+        wj22.setNombre("Pedro");
+        Player wj32 = new Player();
+        wj32.setNombre("Pedro");
+        Player wj42 = new Player();
+        wj42.setNombre("Pedro");
+        ganadores2.Iniciar(wj12, wj22, wj32, wj42);
+        Team perdedores2 = new Team("Pedro","Quillami", 12);
+        Player pj12 = new Player();
+        pj12.setNombre("Pedro");
+        Player pj22 = new Player();
+        pj22.setNombre("Pedro");
+        Player pj32 = new Player();
+        pj32.setNombre("Pedro");
+        Player pj42 = new Player();
+        pj42.setNombre("Pedro");
+        perdedores2.Iniciar(pj12, pj22, pj32, pj42);
 
         JFileChooser fc = new JFileChooser(System.getProperty("user.dir"));
+        fc.showOpenDialog(fc);
         File f = fc.getSelectedFile();
+
+        Result r = new Result(ganadores, perdedores);
+        Result r2  = new Result(ganadores2,perdedores2);
+        ArrayList<Result> cositas = new ArrayList<>();
+        cositas.add(r);
+        cositas.add(r2);
+
+        for (Result aux : cositas) {
+            System.out.println(aux.getEquipoWin().getName());
+            System.out.println(aux.getEquipoLose().getName());
+
+        }
+        
+
+        controlArchivos prueba = new controlArchivos();
+        prueba.guardarResultado(cositas, f);
+        prueba.leerTodo();
     }
     
 }
--- target/classes/controller/controlArchivos.java
+++ target/classes/controller/controlArchivos.java
@@ -16,7 +16,7 @@ public class controlArchivos {
     private ArrayList<Team> equipos;
     private Properties prop = new Properties();
     private RandomAccessFile archivo;
-    private long tamreg = 145; //4(Clave) 4(3 Equipo) 5x25(Nombre - Equipo y jugadores) 12(Resultado) = 
+    private long tamreg = 270; // 4(Clave) 4(3 Equipo) 5x25(Nombre - Equipo y jugadores) 12(Resultado) =
 
     public controlArchivos() throws IOException {
         this.equipos = new ArrayList<>();
@@ -45,75 +45,100 @@ public class controlArchivos {
 
     public void guardarResultado(ArrayList<Result> resultados, File f) {
         try {
-            archivo = new RandomAccessFile(f, "rw");
+            this.archivo = new RandomAccessFile(f,"rw");
+            int clave = buscarUltimo();
             for (Result aux : resultados) {
-                // Datos ganadores
-                archivo.writeInt(buscarUltimo()+1);
-
-                int numWin = aux.getEquipoWin().getTeamNumber();
-                archivo.writeInt(numWin);
-
-                String nWin = aux.getEquipoWin().getName();
-                if (nWin.length() < 25) {
-                    for (int i = nWin.length(); i < 25; i++) {
-                        nWin = nWin + " ";
-                    }
-                } else {
-                    nWin = nWin.substring(0, 25);
-                }
-                archivo.writeChars(nWin);
-
-                ArrayList<Player> playersWin = aux.getEquipoWin().getPlayers();
-                for (Player p : playersWin) {
-                    String j = p.getNombre();
-                    if (j.length() < 25) {
-                        for (int i = j.length(); i < 25; i++) {
-                            j = j + " ";
-                        }
-                    } else {
-                        j = j.substring(0, 25);
-                    }
-                    archivo.writeChars(j);
-                }
-                String r1 = "Gano  ";
-                archivo.writeChars(r1);
-
-                // Datos perdedores
-                archivo.writeInt(buscarUltimo()+1);
-
-                int numLose = aux.getEquipoLose().getTeamNumber();
-                archivo.writeInt(numLose);
-
-                String nLose = aux.getEquipoLose().getName();
-                if (nLose.length() < 25) {
-                    for (int i = nLose.length(); i < 25; i++) {
-                        nLose = nLose + " ";
-                    }
-                } else {
-                    nLose = nLose.substring(0, 25);
-                }
-                archivo.writeChars(nLose);
-
-                ArrayList<Player> playersLose = aux.getEquipoLose().getPlayers();
-                for (Player p : playersLose) {
-                    String j = p.getNombre();
-                    if (j.length() < 25) {
-                        for (int i = j.length(); i < 25; i++) {
-                            j = j + " ";
-                        }
-                    } else {
-                        j = j.substring(0, 25);
-                    }
-                    archivo.writeChars(j);
-                }
-                String r2 = "Perdio";
-                archivo.writeChars(r2);
+                clave++;
+                guardarCampo(aux.getEquipoWin(), true, clave);
+                clave++;
+                guardarCampo(aux.getEquipoLose(), false, clave);
             }
 
         } catch (Exception e) {
-            // TODO: handle exception
+            e.printStackTrace();
         }
 
+    }
+
+    public void leerTodo() throws IOException {
+        System.out.println("\nMostrando todos los Registros");;
+        try {
+            archivo.seek(0);
+            long canreg = archivo.length() / tamreg;
+            for (int r = 0; r < canreg; r++) {
+                int clave = archivo.readInt();
+                int numE = archivo.readInt();
+                String nombreE = "";
+                String nombrej1 = "";
+                String nombrej2 = "";
+                String nombrej3 = "";
+                String nombrej4 = "";
+                String resultado = "";
+                for (int i = 0; i < 25; ++i) {
+                    nombreE += archivo.readChar();
+                }
+                for (int i = 0; i < 25; ++i) {
+                    nombrej1 += archivo.readChar();
+                }
+                for (int i = 0; i < 25; ++i) {
+                    nombrej2 += archivo.readChar();
+                }
+                for (int i = 0; i < 25; ++i) {
+                    nombrej3 += archivo.readChar();
+                }
+                for (int i = 0; i < 25; ++i) {
+                    nombrej4 += archivo.readChar();
+                }
+                for (int i = 0; i < 6; ++i) {
+                    resultado += archivo.readChar();
+                }
+
+                nombreE = quitarEspacios(nombreE);
+                nombrej1 = quitarEspacios(nombrej1);
+                nombrej2 = quitarEspacios(nombrej2);
+                nombrej3 = quitarEspacios(nombrej3);
+                nombrej4 = quitarEspacios(nombrej4);
+
+                System.out.println("Clave: "+clave+" |  # de Equipo: "+numE+" |  Nombre de equipo: "+nombreE+" |  Jugador #1: "+ nombrej1+" |  Jugador #1: "
+                            +nombrej1+" |  Jugador #2: "+nombrej2+" |  Jugador #3: "+nombrej3+" |  Jugador #4: "+nombrej4);
+
+            }
+        } catch (FileNotFoundException fnfe) {/* Archivo no encontrado */
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+    }
+
+    public ArrayList<Team> getEquipos() {
+        return this.equipos;
+    }
+
+    private void guardarCampo(Team datos, boolean win, int clave){
+        try{
+            if (archivo.length() != 0) {
+                archivo.seek(archivo.length());
+            }
+            archivo.writeInt(clave);
+
+            int numWin = datos.getTeamNumber();
+            archivo.writeInt(numWin);
+
+            String nombreEquipo = llenarEspacios(datos.getName());
+            archivo.writeChars(nombreEquipo);
+
+            ArrayList<Player> players = datos.getPlayers();
+            for (Player p : players) {
+                String j = llenarEspacios(p.getNombre());
+                archivo.writeChars(j);
+            }
+            
+            if(win) archivo.writeChars("Gano  ");
+            else    archivo.writeChars("Perdio");
+            
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 
     private int buscarUltimo() {
@@ -128,8 +153,19 @@ public class controlArchivos {
         return clave;
     }
 
-    public ArrayList<Team> getEquipos() {
-        return this.equipos;
+    private String llenarEspacios(String x){
+        if (x.length() < 25) {
+            for (int i = x.length(); i < 25; i++) {
+                x = x + " ";
+            }
+        } else {
+            x = x.substring(0, 25);
+        }
+        return x;
     }
+
+    private String quitarEspacios(String x){
+        return x.split("\\  ")[0];
+    } 
 
 }
