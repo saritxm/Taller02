90e1a8632c61a992a8b17eb069937e143a82573b
saritxm <saranicollemm@gmail.com>
2024-03-09 23:31:26 -0500

    nose

--- src/main/java/view/vPartida.java
+++ src/main/java/view/vPartida.java
@@ -14,6 +14,7 @@ public class vPartida extends javax.swing.JFrame {
      * Creates new form vPartida
      */
     public vPartida() {
+        setTitle("Partida de tejo");
         initComponents();
     }
 
