a3968792c827389fcaa6583963f9416593e5cad4
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-03-10 18:53:14 -0500

    aaa

--- src/main/java/controller/controlMain.java
+++ src/main/java/controller/controlMain.java
@@ -35,17 +35,13 @@ public class controlMain implements ActionListener {
         this.vb.btnContinuar.addActionListener(this);
         this.vb.btnSalir1.addActionListener(this);
         this.vj.btnRegistrarJ.addActionListener(this);
-<<<<<<< HEAD
         this.vj.btnSalirvJ.addActionListener(this);
         this.vj2.btnSalirvJv2.addActionListener(this);
         this.vj2.btnRegistrarJv2.addActionListener(this);
         this.vp.btnSalirP.addActionListener(this);
         this.vp.btnLanzartejo.addActionListener(this);
-        
-=======
         //Controladores
         cArchivos = new controlArchivos();
->>>>>>> deccddcd671e65dbd0fb19900c2f9a5b6370ead2
         //Instanciacion del arraylist de los jugadores que se registran
         this.players = new ArrayList<Player>();
         iniciar();
@@ -53,6 +49,52 @@ public class controlMain implements ActionListener {
     private void iniciar() {
         vb.setVisible(true);
     }
+
+    private void crearJugadores(){
+        this.p1 = new Player();
+        this.p2 = new Player();
+        this.p3 = new Player();
+        this.p4 = new Player();
+        this.p5 = new Player();
+        this.p6 = new Player();
+        this.p7 = new Player();
+        this.p8 = new Player();
+    }
+    private void asignarDatosplayers(){
+        p1.setNombre(vj.cajaNombreJ1.getText());
+        p1.setEdad(Integer.parseInt(vj.cajaEdadJ1.getText()));
+        p1.setCedula(vj.cajaCedulaJ1.getText());
+        p2.setNombre(vj.cajaNombreJ2.getText());
+        p2.setEdad(Integer.parseInt(vj.cajaEdadJ2.getText()));
+        p2.setCedula(vj.cajaCedulaJ2.getText());
+        p3.setNombre(vj.cajaNombreJ3.getText());
+        p3.setEdad(Integer.parseInt(vj.cajaEdadJ3.getText()));
+        p3.setCedula(vj.cajaCedulaJ3.getText());
+        p4.setNombre(vj.cajaNombreJ4.getText());
+        p4.setEdad(Integer.parseInt(vj.cajaEdadJ4.getText()));
+        p4.setCedula(vj.cajaCedulaJ4.getText());
+        p5.setNombre(vj.cajaNombreJ5.getText());
+        p5.setEdad(Integer.parseInt(vj.cajaEdadJ5.getText()));
+        p5.setCedula(vj.cajaCedulaJ5.getText());
+        p6.setNombre(vj.cajaNombreJ6.getText());
+        p6.setEdad(Integer.parseInt(vj.cajaEdadJ6.getText()));
+        p6.setCedula(vj.cajaCedulaJ6.getText());
+        p7.setNombre(vj.cajaNombreJ7.getText());
+        p7.setEdad(Integer.parseInt(vj.cajaEdadJ7.getText()));
+        p7.setCedula(vj.cajaCedulaJ7.getText());
+        p8.setNombre(vj.cajaNombreJ8.getText());
+        p8.setEdad(Integer.parseInt(vj.cajaEdadJ8.getText()));
+        p8.setCedula(vj.cajaCedulaJ8.getText());
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        players.add(p4);
+        players.add(p5);
+        players.add(p6);
+        players.add(p7);
+        players.add(p8);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == this.vb.btnContinuar){
@@ -66,38 +108,8 @@ public class controlMain implements ActionListener {
             this.vj2.dispose();
         }
         if(e.getSource() == this.vj.btnRegistrarJ){
-            p1.setNombre(vj.cajaNombreJ1.getText());
-            p1.setEdad(Integer.parseInt(vj.cajaEdadJ1.getText()));
-            p1.setCedula(vj.cajaCedulaJ1.getText());
-            p2.setNombre(vj.cajaNombreJ2.getText());
-            p2.setEdad(Integer.parseInt(vj.cajaEdadJ2.getText()));
-            p2.setCedula(vj.cajaCedulaJ2.getText());
-            p3.setNombre(vj.cajaNombreJ3.getText());
-            p3.setEdad(Integer.parseInt(vj.cajaEdadJ3.getText()));
-            p3.setCedula(vj.cajaCedulaJ3.getText());
-            p4.setNombre(vj.cajaNombreJ4.getText());
-            p4.setEdad(Integer.parseInt(vj.cajaEdadJ4.getText()));
-            p4.setCedula(vj.cajaCedulaJ4.getText());
-            p5.setNombre(vj.cajaNombreJ5.getText());
-            p5.setEdad(Integer.parseInt(vj.cajaEdadJ5.getText()));
-            p5.setCedula(vj.cajaCedulaJ5.getText());
-            p6.setNombre(vj.cajaNombreJ6.getText());
-            p6.setEdad(Integer.parseInt(vj.cajaEdadJ6.getText()));
-            p6.setCedula(vj.cajaCedulaJ6.getText());
-            p7.setNombre(vj.cajaNombreJ7.getText());
-            p7.setEdad(Integer.parseInt(vj.cajaEdadJ7.getText()));
-            p7.setCedula(vj.cajaCedulaJ7.getText());
-            p8.setNombre(vj.cajaNombreJ8.getText());
-            p8.setEdad(Integer.parseInt(vj.cajaEdadJ8.getText()));
-            p8.setCedula(vj.cajaCedulaJ8.getText());
-            players.add(p1);
-            players.add(p2);
-            players.add(p3);
-            players.add(p4);
-            players.add(p5);
-            players.add(p6);
-            players.add(p7);
-            players.add(p8);
+            crearJugadores();
+            asignarDatosplayers();
             cPlayers.randomPlayers(players);
            }
     }
