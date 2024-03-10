2776bdcd75b2a1722743cfb3fc9f323c79f722c5
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-03-09 21:05:02 -0500

    as

--- target/classes/controller/controlArchivos.java
+++ target/classes/controller/controlArchivos.java
@@ -1,9 +1,26 @@
 package controller;
 
-public class controlArchivos {
-    private ArrayList<>
-    public controlArchivos(){
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
 
+import model.Team;
+
+public class controlArchivos {
+    private ArrayList<Team> equipos;
+    private Properties prop = new Properties();
+
+    public controlArchivos(){
+        this.equipos = new ArrayList<>();
+    }
+
+    public void obtenerEquipos(String ruta) throws IOException{
+        try {
+            prop.load(new FileInputStream("data/teams.properties"));
+        } catch (IOException e) {
+            e.getStackTrace();        
+        }
     }
     
 }
