159dd0912142aef030147860ee0063e3bc202b51
saritxm <saranicollemm@gmail.com>
2024-03-09 21:34:05 -0500

    control

--- src/main/java/controller/controlMain.java
+++ src/main/java/controller/controlMain.java
@@ -1,5 +1,23 @@
 package controller;
 
-public class controlMain {
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.vBienvenida;
+
+public class controlMain implements ActionListener {
+
+    private vBienvenida vb;
+    private controlArchivos cArchivos;
+    private controlPlayers cPlayers;
+
+    public controlMain() {
+        this.vb = new vBienvenida();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
 }
