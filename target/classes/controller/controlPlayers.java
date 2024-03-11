d51b555dec857e5ea4fb837306225c2f2b4cd8fa
Drack678 <marlonyesid33@gmail.com>
2024-03-10 21:11:08 -0500

    .

--- target/classes/controller/controlPlayers.java
+++ target/classes/controller/controlPlayers.java
@@ -76,10 +76,4 @@ public class controlPlayers {
     public Team getEquipo2() {
         return this.equipo2;
     }
-
-    public int play(Team t){
-        for(Player p: t.getPlayers()){
-            return p.lanzarTejo();
-        }
-    }
 }
