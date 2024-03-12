8957742a24348dd422d1a2452944913c1e3d17b1
Drack678 <marlonyesid33@gmail.com>
2024-03-11 22:14:20 -0500

    Modifique algunas privacidades y hice algunas pruebas con el metodo cambiar opacidad

--- src/main/java/view/vPartida.java
+++ src/main/java/view/vPartida.java
@@ -5,6 +5,7 @@
 package view;
 
 import javax.swing.JOptionPane;
+import java.awt.Color;
 
 /**
  *
@@ -93,7 +94,7 @@ public class vPartida extends javax.swing.JFrame {
         panel1.add(jLabel1A, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 40, 190, 20));
 
         jLabel2A.setText("jLabel5");
-        panel1.add(jLabel2A, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 110, 170, -1));
+        panel1.add(jLabel2A, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 110, 170, 20));
 
         jLabel3A.setText("jLabel5");
         panel1.add(jLabel3A, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 180, 180, -1));
@@ -118,16 +119,16 @@ public class vPartida extends javax.swing.JFrame {
     public javax.swing.JButton btnLanzartejo;
     public javax.swing.JButton btnSalirP;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel1A;
+    public javax.swing.JLabel jLabel1A;
     private javax.swing.JLabel jLabel1B;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel2A;
+    public javax.swing.JLabel jLabel2A;
     private javax.swing.JLabel jLabel2B;
-    private javax.swing.JLabel jLabel3A;
+    public javax.swing.JLabel jLabel3A;
     private javax.swing.JLabel jLabel3B;
-    private javax.swing.JLabel jLabel4A;
+    public javax.swing.JLabel jLabel4A;
     private javax.swing.JLabel jLabel4B;
-    private javax.swing.JLabel jLabelFondoA;
+    public javax.swing.JLabel jLabelFondoA;
     private javax.swing.JLabel jLabelFondoB;
     private javax.swing.JPanel jPanel1;
     private java.awt.Panel panel1;
@@ -138,5 +139,9 @@ public class vPartida extends javax.swing.JFrame {
     }
     public void mostrarResultado(String jugadores){
         JOptionPane.showMessageDialog(null, jugadores, "EQUIPO GANADOR", JOptionPane.INFORMATION_MESSAGE);
+    }
+    public void cambiarOpacidad(){
+        Color backGroundcolor = new Color(jLabel1A.getBackground().getRed(),jLabel1A.getBackground().getGreen(),jLabel1A.getBackground().getBlue(),128);
+        jLabel1A.setBackground(backGroundcolor);
     }
 }
